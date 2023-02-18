2804f57faaa6518c91d76e1776bee112e9de9808
masaduzzaman319 <74820206+masaduzzaman319@users.noreply.github.com>
2023-02-18 11:11:53 +0600

    First commit

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-import phonenumbers
-
-from number import numbers
-
-from phonenumbers import geocoder
-from phonenumbers import carrier
-
-country = phonenumbers.parse(numbers, "CH")
-operater = phonenumbers.parse(numbers, "RO")
-
-print (geocoder.description_for_number(country, "en"))
-print (carrier.name_for_number(operater, "en"))
+print('asad')
